567969dd01bb557e6e1ba6018759d656c120c1eb
Julie Yohn <julie.yohn@student.swic.edu>
2019-10-09 20:46:41 -0500

    Move GLOBAL FUNCTIONS into an OBJECT LITERAL.

--- scripts.js
+++ scripts.js
@@ -1,28 +1,29 @@
 // Simple arithmetic calculator app.
 
 /**
- * 3. Log the value of the select and inputs.
+ * Use BRACKET NOTATION to INVOKE the APPROPRIATE METHOD.
  */
 
-function add(a, b) {
-  return Number(a) + Number(b);
-}
+const calculator = {
+  add(a, b) {
+    return Number(a) + Number(b);
+  },
+  subtract(a, b) {
+    return a - b;
+  },
+  multiply(a, b) {
+    return a * b;
+  },
 
-function subtract(a, b) {
-  return a - b;
-}
+  divide(a, b) {
+    return a / b;
+  },
+  remainder(a, b) {
+    return a % b;
+  }
+};
+console.log(calculator.add(1, 5));
 
-function multiply(a, b) {
-  return a * b;
-}
-
-function divide(a, b) {
-  return a / b;
-}
-
-function remainder(a, b) {
-  return a % b;
-}
 const form = document.querySelector("form");
 const num1 = document.querySelector("#num-1");
 const num2 = document.querySelector("#num-2");
