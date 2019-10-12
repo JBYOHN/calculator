e062899af829eba3ee59ce30c3d7905e8e7e1d05
Julie Yohn <julie.yohn@student.swic.edu>
2019-10-12 13:02:26 -0500

    Use BRACKET NOTATION to INVOKE the APPROPRIATE METHOD.

--- scripts.js
+++ scripts.js
@@ -1,9 +1,5 @@
 // Simple arithmetic calculator app.
 
-/**
- * Use BRACKET NOTATION to INVOKE the APPROPRIATE METHOD.
- */
-
 const calculator = {
   add(a, b) {
     return Number(a) + Number(b);
@@ -22,12 +18,12 @@ const calculator = {
     return a % b;
   }
 };
-console.log(calculator.add(1, 5));
 
 const form = document.querySelector("form");
 const num1 = document.querySelector("#num-1");
 const num2 = document.querySelector("#num-2");
 const select = document.querySelector("select");
+const output = document.querySelector("output");
 
 //Use an anonymous callback function as the 2nd argument.
 form.addEventListener("submit", function(event) {
@@ -35,5 +31,6 @@ form.addEventListener("submit", function(event) {
   event.preventDefault();
 
   //form inputs have a property called value.
-  console.log(num1.value, num2.value, select.value);
+
+  output.textContent = calculator[select.value](num1.value, num2.value);
 });
